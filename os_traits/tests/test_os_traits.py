71e35dee0a4642677b3926dc4af48adea94cb329
jianghua wang <jianghua.wang@citrix.com>
2017-05-10 04:51:14 +0100

    GPU: add os traits for GPU - resolution
    Different GPUs (or vGPUs) may support different resolutions.
    This commit is to define traits for differentiate the resolution
    for GPUs (or vGPUs).
    
    Change-Id: I41c0bf6e17c2a2b832f251b95b5f2be7b3d5e8e9

--- os_traits/tests/test_os_traits.py
+++ os_traits/tests/test_os_traits.py
@@ -17,6 +17,7 @@ import re
 import os_traits as ot
 from os_traits.hw.cpu import x86
 from os_traits.hw.gpu import api
+from os_traits.hw.gpu import resolution
 from os_traits.hw.nic import offload
 from os_traits.tests import base
 
@@ -33,6 +34,8 @@ class TestSymbols(base.TestCase):
         # And the "leaf-module" namespace...
         self.assertEqual(x86.SSE42, ot.HW_CPU_X86_SSE42)
         self.assertEqual(api.DIRECTX_V10, ot.HW_GPU_API_DIRECTX_V10)
+        self.assertEqual(resolution.W1920H1080,
+                         ot.HW_GPU_RESOLUTION_W1920H1080)
         self.assertEqual(offload.TSO, ot.HW_NIC_OFFLOAD_TSO)
 
     def test_get_traits(self):
